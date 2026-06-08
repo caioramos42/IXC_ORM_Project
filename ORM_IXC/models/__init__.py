ba7d3ea034b8480c7fccd490e5a53776ddff649c
caio.ramos42 <caio.ramos@brasillike.com.br>
2026-06-08 17:46:49 -0300

    add init

--- ORM_IXC/models/__init__.py
+++ ORM_IXC/models/__init__.py
@@ -1,11 +1,22 @@
 """Modelos de dados para ORM IXC."""
-
+from tableModels.atendimentoModel import AtendimentoModel
+from tableModels.clienteModel import ClientModel
+from tableModels.defaultModel import DefaultPayload
+from tableModels.loginModel import LoginModel
+from tableModels.serviceOrderModel import ServiceOrderModel
+from tableModels.clienteModel import ClientModel
+from tableModels.loginModel import LoginModel
+from tableModels.serviceOrderModel import ServiceOrderModel
+from tableModels.contasAReceber import ContasAReceberModel
+from tableModels.contratoDoClienteModel import ContratoDoClienteModel
 __all__ = [
     "ClientModel",
     "DefaultPayload",
     "LoginModel",
     "ServiceOrderModel",
-    "Client",
-    "Login",
-    "OrdemDeServico",
+    "ClientModel",
+    "ServiceOrderModel",
+    "AtendimentoModel",
+    "ContasAReceberModel",
+    "ContratoDoClienteModel"
 ]
