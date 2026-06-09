3eb5166e805acaa5e357c4c5648209003107bf38
caio.ramos42 <caio.ramos@brasillike.com.br>
2026-06-09 08:08:31 -0300

    add init

---
+++ ORM_IXC/statemants/__init__.py
@@ -0,0 +1,11 @@
+from .CRUD.delete import delete
+from .CRUD.insert import insert
+from .CRUD.select import select
+from .CRUD.update import update
+
+__all__ = [
+    "delete",
+    "insert",
+    "select",
+    "update"
+]
