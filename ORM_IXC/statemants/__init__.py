098c9347a2d6ba4f3cb43ef2100747cf351e8faf
caio.ramos42 <caio.ramos@brasillike.com.br>
2026-06-09 08:22:02 -0300

    add init

--- ORM_IXC/statemants/__init__.py
+++ ORM_IXC/statemants/__init__.py
@@ -2,10 +2,12 @@ from .CRUD.delete import delete
 from .CRUD.insert import insert
 from .CRUD.select import select
 from .CRUD.update import update
+from .maps.mapper import Mapped
 
 __all__ = [
     "delete",
     "insert",
     "select",
-    "update"
+    "update",
+    "Mapped"
 ]
